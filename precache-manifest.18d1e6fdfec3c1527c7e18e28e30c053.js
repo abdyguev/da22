c85983d7b70f04f3ecba9d7c717d7ba410ba3eaf
“Abdy <abdyguev@gmail.com>
2020-08-13 17:13:09 -0400

    Updates

---
+++ precache-manifest.18d1e6fdfec3c1527c7e18e28e30c053.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a3f56ac22db79e2d72c3d78934321a",
+    "url": "/da22/index.html"
+  },
+  {
+    "revision": "235cc62ee99749fec343",
+    "url": "/da22/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "7f776c9d369c8ddd417b",
+    "url": "/da22/static/css/main.65a83612.chunk.css"
+  },
+  {
+    "revision": "235cc62ee99749fec343",
+    "url": "/da22/static/js/2.42a82582.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/da22/static/js/2.42a82582.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f776c9d369c8ddd417b",
+    "url": "/da22/static/js/main.10fa0abc.chunk.js"
+  },
+  {
+    "revision": "314844adaf101279a3b4",
+    "url": "/da22/static/js/runtime-main.47f1a51c.js"
+  }
+]);
